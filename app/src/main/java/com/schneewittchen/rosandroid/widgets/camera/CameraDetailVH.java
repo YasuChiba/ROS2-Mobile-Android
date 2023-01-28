e2693c866e5ec216e9b46d7a3b43f8d3df6ac501
YasuChiba <yasu.bv0309@gmail.com>
2023-01-29 01:41:59 +0900

    add camera widget

---
+++ app/src/main/java/com/schneewittchen/rosandroid/widgets/camera/CameraDetailVH.java
@@ -0,0 +1,52 @@
+package com.schneewittchen.rosandroid.widgets.camera;
+
+import android.view.View;
+
+import com.schneewittchen.rosandroid.model.entities.widgets.BaseEntity;
+import com.schneewittchen.rosandroid.ui.views.details.SubscriberWidgetViewHolder;
+
+import java.util.Arrays;
+import java.util.List;
+
+import sensor_msgs.msg.CompressedImage;
+import sensor_msgs.msg.Image;
+
+/**
+ * TODO: Description
+ *
+ * @author Nils Rottmann
+ * @version 1.0.0
+ * @created on 13.05.20
+ * @updated on 07.09.20
+ * @modified by Nico Studt
+ * @updated on 17.09.20
+ * @modified by Nils Rottmann
+ * @updated on 20.03.21
+ * @modified by Nico Studt
+ */
+public class CameraDetailVH extends SubscriberWidgetViewHolder {
+
+    public static final String TAG = CameraDetailVH.class.getSimpleName();
+
+
+    @Override
+    protected void initView(View parentView) {
+
+    }
+
+    @Override
+    protected void bindEntity(BaseEntity entity) {
+
+    }
+
+    @Override
+    protected void updateEntity(BaseEntity entity) {
+
+    }
+
+    @Override
+    public List<String> getTopicTypes() {
+        return Arrays.asList(Image.class.getCanonicalName(), CompressedImage.class.getCanonicalName());
+    }
+
+}
